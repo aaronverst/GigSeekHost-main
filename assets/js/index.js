d3709d7859b0ea628128a79ba6eb03e8f0aa4027
Aaron <aaronverst@hotmail.com>
2023-03-23 12:15:01 -0400

    Updated Cards

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,10 +20,6 @@ const cardData = [
         body: 'this is card body 5'
     },
     {
-        name: 'card 5',
-        body: 'this is card body 5'
-    },
-    {
         name: 'card 6',
         body: 'this is card body 6'
     },
@@ -44,22 +40,21 @@ const cardMethods = () => {
         const postElement = document.createElement('div');
         postElement.classList.add('our-venue');
         postElement.innerHTML = `
-            <div class="our-venue">
+
               <div class="picture">
-                <img class="img-fluid" src="https://picsum.photos/130/130?image=1027">
+                <img class="img-fluid" src="https://source.unsplash.com/random/800x800/?img=1">
               </div>
               <div class="team-content">
-                <h3 class="name">Michele Miller</h3>
-                <h4 class="title">Web Developer</h4>
+                <h3 class="name">${postData.name}</h3>
+                <h4 class="title">${postData.body}</h4>
               </div>
               <ul class="social">
                 <li><a href="https://codepen.io/collection/XdWJOQ/" class="fa fa-facebook" aria-hidden="true"></a></li>
                 <li><a href="https://codepen.io/collection/XdWJOQ/" class="fa fa-twitter" aria-hidden="true"></a></li>
-                <li><a href="https://codepen.io/collection/XdWJOQ/" class="fa fa-google-plus" aria-hidden="true"></a>
-                </li>
+                <li><a href="https://codepen.io/collection/XdWJOQ/" class="fa fa-google-plus" aria-hidden="true"></a></li>
                 <li><a href="https://codepen.io/collection/XdWJOQ/" class="fa fa-linkedin" aria-hidden="true"></a></li>
               </ul>
-            </div>
+            
         `
         postcontainer.appendChild(postElement)
     })
