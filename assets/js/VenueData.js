f90636e496bb293eca2418995cab4144f24b4d14
Aaron <aaronverst@hotmail.com>
2023-03-23 16:43:44 -0400

    Added Logo And Started filter bar

--- assets/js/VenueData.js
+++ assets/js/VenueData.js
@@ -14,9 +14,45 @@ d3.csv('../assets/data/VenueData.csv')
         };
 
         console.log(cardData);
+
+
+        const nameContainer = document.querySelector('.search-venue');
+
+        const nameMethods = () => {
+            cardData.map((nameData) => {
+                const nameElement = document.createElement('div');
+                nameElement.classList.add('dropdown-content');
+                nameElement.innerHTML = `
+                <a href="#Name0">${nameData.Name}</a>           
+        `
+                nameContainer.appendChild(nameElement);
+            })
+        }
+        nameMethods()
+
     })
 
     .catch(error => console.error(error));
+
+function Dropdown() {
+    document.getElementById("nameDropdown").classList.toggle("show");
+};
+
+function nameFilterFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("nameInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("nameDropdown");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+        txtValue = a[i].textContent || a[i].innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+        } else {
+            a[i].style.display = "none";
+        }
+    }
+};
 
 
 
